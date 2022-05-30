d38e89b5989c94d090b9a57987ac664642ab6763
nyw2020 <nyw@amazon.com>
2022-05-30 12:16:20 -0400

    added lambda and apigateway, pretty easily, learned about npm run watch for compiling typescript and cdk watch for hot swapping dev stacks

--- lib/cdk-helloworld-stack.ts
+++ lib/cdk-helloworld-stack.ts
@@ -1,16 +1,19 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { App, Stack, StackProps } from "aws-cdk-lib";
+import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
+import { LambdaRestApi } from "aws-cdk-lib/aws-apigateway";
 
 export class CdkHelloworldStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: App, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const helloWorld = new Function(this, "HelloHandler", {
+      runtime: Runtime.NODEJS_16_X,
+      code: Code.fromAsset("lambda"), // file path
+      handler: "hello.handler", // handler
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkHelloworldQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new LambdaRestApi(this, "Endpoint", {
+      handler: helloWorld,
+    });
   }
 }
